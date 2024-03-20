ba14393891b0281f654a977636bbc3e19b66183a
Sergei Koloskov <ser.koloskov13@gmail.com>
2024-03-20 23:05:17 +0300

    Hard part is done

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,24 +7,84 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var CounterLabel: UILabel!
     
+    @IBOutlet weak var logsTextView: UITextView!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var substractOneButton: UIButton!
     @IBOutlet weak var addOneButton: UIButton!
-    private var number = 0
+    
+    
+    private var today = Date.now
+    let dateFormatter = DateFormatter()
+    var trueDate = ""
+    private var isResseted = false
+    private var lessThanZero = false
+    
+    private var number = 0 {
+        didSet {
+            if number < 0 {
+                number += 1
+            }
+            today = Date.now
+            trueDate = dateFormatter.string(from: today)
+            CounterLabel.text = "Значение счетчика: \(number)"
+            printLogsInTextView(oldValue: oldValue)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        CounterLabel.text = "Значени счетчика: \(number)"
+        logsTextView.text = "История изменений:"
+        CounterLabel.text = "Значение счетчика: \(number)"
+        dateFormatter.dateFormat = "HH:mm:ss, dd/MM/yyyy"
+        
         addOneButton.layer.cornerRadius = 16
+        substractOneButton.layer.cornerRadius = 16
+        resetButton.layer.cornerRadius = 12
     }
 
 
     @IBAction func AddOne(_ sender: Any) {
         number += 1
-        CounterLabel.text = "Значени счетчика: \(number)"
     }
+    
+    @IBAction func minusOne(_ sender: Any) {
+        if number == 0 {
+            lessThanZero = true
+        }
+        number -= 1
+    }
+    
+    @IBAction func resetTheCounter(_ sender: Any) {
+        isResseted = true
+        number = 0
+    }
+    
+    func printLogsInTextView(oldValue: Int) {
+        
+        if lessThanZero {
+            logsTextView.text += "\n\(trueDate): попытка уменьшить значение счётчика ниже 0"
+            lessThanZero = false
+        } else {
+            if isResseted {
+                logsTextView.text += "\n\(trueDate): значение сброшено"
+                isResseted = false
+            } else {
+                if oldValue < number {
+                    logsTextView.text += "\n\(trueDate): значение изменено на +1"
+                } else {
+                    logsTextView.text += "\n\(trueDate): значение изменено на -1"
+                }
+            }
+        }
+        let range = NSRange(location: logsTextView.text.count - 1, length: 0)
+        logsTextView.scrollRangeToVisible(range)
+    }
+    
     
 }
 
